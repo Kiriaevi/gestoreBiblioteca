bc59607a19baf0b315b1aa3eef31df40dae90fd7
Kiriaevi <fra977502@gmail.com>
2025-05-16 20:52:12 +0200

    Prima possibile implementazione di LibreriaPersistenteJSON

--- src/main/java/loadingLibreria/LibreriaPersistenteJSON.java
+++ src/main/java/loadingLibreria/LibreriaPersistenteJSON.java
@@ -2,9 +2,43 @@ package loadingLibreria;
 
 import entities.Libro;
 
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.stream.JsonGenerator;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class LibreriaPersistenteJSON implements LibreriaPersistente{
+public class LibreriaPersistenteJSON extends LibreriaPersistenteAbstract{
+
+	private final String fileName = "libri.json";
+	private JsonGenerator jsonGen = null;
+	private JsonReader jsonReader = null;
+	@Override
+	public boolean onInit() {
+		try {
+			if (jsonGen == null)
+				jsonGen = Json.createGenerator(new FileWriter(fileName));
+			if (jsonReader == null)
+				jsonReader = Json.createReader(Files.newInputStream(Paths.get(fileName)));
+		} catch (IOException e) {
+			System.out.println("Errore nell'inizializzazione del componente JSON: " + e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void onClose() {
+		if(jsonGen != null) {
+			jsonGen.close();
+		}
+		if(jsonReader != null) {
+			jsonReader.close();
+		}
+	}
 
 	@Override
 	public ArrayList<Libro> leggiLibro(int size) {
@@ -22,5 +56,15 @@ public class LibreriaPersistenteJSON implements LibreriaPersistente{
 	public boolean eliminaLibro(Libro libro) {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'eliminaLibro'");
+	}
+
+	@Override
+	public String salvaLibro(Libro libro) {
+        return null;
+    }
+
+	@Override
+	public int getSize() {
+		return 0;
 	}
 }
---
+++ src/main/java/loadingLibreria/LibreriaPersistenteProxy.java
@@ -0,0 +1,5 @@
+package loadingLibreria;
+
+public class LibreriaPersistenteProxy {
+
+}
