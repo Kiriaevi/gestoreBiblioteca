1892a438f6de48240e97c2dd8ef5f522a6b08bbf
Kiriaevi <fra977502@gmail.com>
2025-05-16 20:51:45 +0200

    Rimosso builder pattern

--- src/main/java/entities/Libro.java
+++ src/main/java/entities/Libro.java
@@ -5,10 +5,7 @@ public class Libro {
     private final int valutazione;
     private final Stato stato;
 
-    public static LibroBuilder builder() {
-        return new LibroBuilder();
-    }
-    private Libro(String titolo, String autore, String ISBN, String genere, int valutazione, Stato stato) {
+    public Libro(String titolo, String autore, String ISBN, String genere, int valutazione, Stato stato) {
         this.titolo = titolo;
         this.autore = autore;
         this.ISBN = ISBN;
@@ -40,41 +37,4 @@ public class Libro {
     public Stato getStato() {
         return stato;
     }
-    public static class LibroBuilder {
-        private String titolo = "";
-        private String autore = "";
-        private String ISBN = "";
-        private String genere = "";
-        private int valutazione = 0;
-        private Stato stato = Stato.DA_LEGGERE;
-
-        public LibroBuilder setTitolo(String titolo) {
-            this.titolo = titolo;
-            return this;
-        }
-        public LibroBuilder setAutore(String autore) {
-            this.autore = autore;
-            return this;
-        }
-        public LibroBuilder setISBN(String ISBN) {
-            this.ISBN = ISBN;
-            return this;
-        }
-        public LibroBuilder setGenere(String genere) {
-            this.genere = genere;
-            return this;
-        }
-        public LibroBuilder setValutazione(int valutazione) {
-            this.valutazione = valutazione;
-            return this;
-        }
-        public LibroBuilder setStato(Stato stato) {
-            this.stato = stato;
-            return this;
-        }
-        public Libro build() {
-            return new Libro(titolo, autore, ISBN, genere, valutazione, stato);
-        }
-    }
-
 }
