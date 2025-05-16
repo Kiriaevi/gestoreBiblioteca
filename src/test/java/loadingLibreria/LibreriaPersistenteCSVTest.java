b6234cb6ab9d347280af936fbe81ea6cc63073b6
Kiriaevi <fra977502@gmail.com>
2025-05-16 20:50:03 +0200

    Prima visione al testing

---
+++ src/test/java/loadingLibreria/LibreriaPersistenteCSVTest.java
@@ -0,0 +1,52 @@
+package loadingLibreria;
+
+import entities.Libro;
+import entities.Stato;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LibreriaPersistenteCSVTest {
+
+    private LibreriaPersistenteCSV libreriaPersistenteCSV;
+    @BeforeEach
+    public void setup() {
+        libreriaPersistenteCSV = new LibreriaPersistenteCSV();
+        libreriaPersistenteCSV.onInit();
+    }
+    @Test
+    public void initializationShouldBeSuccessful() {
+        LibreriaPersistenteCSV istanza = new LibreriaPersistenteCSV();
+        assertTrue(istanza.onInit());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {Integer.MIN_VALUE, Integer.MAX_VALUE, 1})
+    public void bookShouldBeSaved(int valutazione) {
+        Libro libro = new Libro(
+                "La legge dell'elefante",
+                "Lirilì",
+                "LUNGOISBN342",
+                "fantasy/brainrot",
+                valutazione,
+                Stato.DA_LEGGERE);
+        assertEquals("La legge dell'elefante,Lirilì,LUNGOISBN342,fantasy/brainrot,"+valutazione+",DA_LEGGERE",
+                libreriaPersistenteCSV.salvaLibro(libro));
+    }
+
+    @Test
+    public void bookShouldBeRead() {
+    }
+
+    @Test
+    public void bookShouldBeUpdated() {
+    }
+
+    @Test
+    public void bookShouldBeDeleted() {
+    }
+}
