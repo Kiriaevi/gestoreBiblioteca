6b31a4b41a522a0f837c881af6bb8cef1ead26ea
Kiriaevi <fra977502@gmail.com>
2025-05-20 20:09:55 +0200

    "Aggiunti file 'libri.json' e 'libri_backup.json', definita la classe Libro come record, aggiornato 'libri.csv'"

--- src/main/java/entities/Libro.java
+++ src/main/java/entities/Libro.java
@@ -1,41 +1,15 @@
 package entities;
 
-public class Libro {
-    private final String titolo, autore, ISBN, genere;
-    private final int valutazione;
-    private final Stato stato;
+import java.io.Serializable;
 
-    public Libro(String titolo, String autore, String ISBN, String genere, int valutazione, Stato stato) {
+public record Libro(String titolo, String autore, String isbn, String genere, int valutazione, Stato stato) implements Serializable {
+    public Libro(String titolo, String autore, String isbn, String genere, int valutazione, Stato stato) {
         this.titolo = titolo.isEmpty() ? "Titolo sconosciuto" : titolo;
         this.autore = autore.isEmpty() ? "Autore sconosciuto" : autore;
-        this.ISBN = ISBN.isEmpty() ? "ISBN non disponibile" : ISBN;
+        this.isbn = isbn.isEmpty() ? "ISBN non disponibile" : isbn;
         this.genere = genere.isEmpty() ? "Genere non impostato" : genere;
         this.valutazione = Math.max(valutazione, 0);
         this.stato = stato == null ? Stato.DA_LEGGERE : stato;
-    }
-
-    public String getTitolo() {
-        return titolo;
-    }
-
-    public String getAutore() {
-        return autore;
-    }
-
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public String getGenere() {
-        return genere;
-    }
-
-    public int getValutazione() {
-        return valutazione;
-    }
-
-    public Stato getStato() {
-        return stato;
     }
 
     @Override
@@ -43,7 +17,7 @@ public class Libro {
         return "Libro{" +
                 "titolo='" + titolo + '\'' +
                 ", autore='" + autore + '\'' +
-                ", ISBN='" + ISBN + '\'' +
+                ", ISBN='" + isbn + '\'' +
                 ", genere='" + genere + '\'' +
                 ", valutazione=" + valutazione +
                 ", stato=" + stato +
