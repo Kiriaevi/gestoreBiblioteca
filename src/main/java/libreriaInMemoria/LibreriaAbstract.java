b40659befeea10cdbe1cfeb647b202afb1a778e3
Kiriaevi <fra977502@gmail.com>
2025-05-21 16:21:21 +0200

    "Refactoring metodi LibreriaAbstract: spostamento `loadAll` e `getLibri` da LibreriaImpl, semplificazione codice e miglioramento struttura"

--- src/main/java/libreriaInMemoria/LibreriaAbstract.java
+++ src/main/java/libreriaInMemoria/LibreriaAbstract.java
@@ -1,8 +1,11 @@
 package libreriaInMemoria;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import comparators.OrdinamentoValutazione;
 import entities.Libro;
 import loadingLibreria.LibreriaPersistente;
 import loadingLibreria.LibreriaPersistenteAbstract;
@@ -15,7 +18,23 @@ public abstract class LibreriaAbstract implements Libreria {
     }
     protected abstract void onInit(String type);
     protected abstract void onClose();
-
+    @Override
+    public void loadAll() {
+        try {
+            libri = lib.leggiLibro(Integer.MAX_VALUE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("IN MEMORIA: "+this.libri);
+    }
+    @Override
+    public Collection<Libro> getLibri(int size) {
+        if (size < 0)
+            throw new IllegalArgumentException("size non può essere un valore negativo");
+        if(size > lib.getSize())
+            return libri.stream().sorted(new OrdinamentoValutazione(true).ottieniComparatore()).toList();
+        return libri.subList(0, size).stream().sorted(new OrdinamentoValutazione(true).ottieniComparatore()).toList();
+    }
     @Override
     public boolean modificaLibro(Libro l, String ISBN) {
         return lib.modificaLibro(l, ISBN);
@@ -28,5 +47,4 @@ public abstract class LibreriaAbstract implements Libreria {
     public void aggiungiLibro(Libro l) {
         lib.salvaLibro(l);
     }
-    public List<Libro> getLibreria() { return this.libri; }
 }
--- src/main/java/libreriaInMemoria/LibreriaImpl.java
+++ src/main/java/libreriaInMemoria/LibreriaImpl.java
@@ -1,15 +1,11 @@
 package libreriaInMemoria;
 
-import java.io.IOException;
-import java.util.Collection;
-import comparators.OrdinamentoValutazione;
-import entities.Libro;
 import loadingLibreria.LibreriaPersistenteCSV;
 import loadingLibreria.LibreriaPersistenteJSON;
 
 public class LibreriaImpl extends LibreriaAbstract {
 
-    private final String nomeFile = "libri";
+    private final static String nomeFile = "libri";
     public LibreriaImpl(String type) {
         super();
         onInit(type);
@@ -28,24 +24,6 @@ public class LibreriaImpl extends LibreriaAbstract {
                 throw new IllegalArgumentException("Sistema compatibile solo con file CSV e JSON");
         }
 	}
-	@Override
-    public void loadAll() {
-        try {
-            super.libri = super.lib.leggiLibro(Integer.MAX_VALUE);
-        } catch (IOException e) {
-            throw new RuntimeException(e+" Errore nella lettura del libro");
-        }
-    }
-
-    @Override
-    public Collection<Libro> getLibri(int size) {
-        if (size < 0)
-            throw new IllegalArgumentException("size non può essere un valore negativo");
-        if(size > super.lib.getSize())
-            return super.libri.stream().sorted(new OrdinamentoValutazione(false).ottieniComparatore()).toList();
-        return super.libri.subList(0, size).stream().sorted(new OrdinamentoValutazione(false).ottieniComparatore()).toList();
-    }
-
     @Override
 	protected void onClose() {
 		// TODO Auto-generated method stub
