99034359252b149c6649739eb22634b1faedfa45
Kiriaevi <fra977502@gmail.com>
2025-05-24 12:09:53 +0200

    "Rinominato il main class da 'gui.Gui' a 'Applicazione' e aggiornati i riferimenti."

--- src/main/java/Applicazione.java
+++ src/main/java/Applicazione.java
@@ -1,28 +1,23 @@
-import entities.Libro;
-import entities.Stato;
-import libreriaInMemoria.Libreria;
+import com.formdev.flatlaf.FlatLightLaf;
+import gui.controller.ControllerLibreria;
+import gui.view.VistaLibreria;
+import libreriaInMemoria.LibreriaAbstract;
 import libreriaInMemoria.LibreriaImpl;
-import org.w3c.dom.ls.LSOutput;
-import ricerca.Filtro;
-import ricerca.FiltroAutore;
-import ricerca.FiltroBase;
-import ricerca.FiltroTitolo;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import javax.swing.*;
 
 public class Applicazione {
-    public static void main(String[] args) {
-        LibreriaImpl libreria = new LibreriaImpl("csv");
-        libreria.loadAll();
-        System.out.println(libreria.getLibri(Integer.MAX_VALUE));
-      //  System.out.println(libreria.getLibri(5));
-        //Libro l = new Libro("学生 の学園", "sus", "12231-2112-12", "brainrot",5, Stato.LETTO);
-        //libreria.aggiungiLibro(l); // <-- non posso richiamare modifica libro
 
-        Collection<Libro> libreriaData = libreria.getLibri(Integer.MAX_VALUE);
-        Filtro filtro = new FiltroAutore(new FiltroTitolo(new FiltroBase(), "viaggio"), "luca");
-        System.out.println(libreriaData.stream().filter(filtro::filtro).toList());
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            System.err.println("FlatLaf not supported");
+        }
+
+        LibreriaAbstract libreria = new LibreriaImpl("json");
+        VistaLibreria v = new VistaLibreria();
+        SwingUtilities.invokeLater(() -> v.setVisible(true));
+        ControllerLibreria c = new ControllerLibreria(libreria, v);
     }
 }
